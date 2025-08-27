23e049970114b77751896265ae8ab06a9b77f277
sohanreddyk <sohan_reddy@outlook.com>
2025-08-27 11:22:22 -0700

    oops1.cpp

---
+++ oops1.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        return f"Hello, my name is {self.name} and I am {self.age} years old."
+
+p1=Person("Alice", 30)
+print(p1.greet())
